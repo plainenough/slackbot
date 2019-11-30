db389275c30ad0686ffedbf675e11b1d4ef65c51
Derrick Walton <derrick@tosstalk.com>
2019-11-30 09:16:56 -0700

    convert fake points dict in kwargs

--- commands/fake_internet_points.py
+++ commands/fake_internet_points.py
@@ -2,11 +2,7 @@
 
 
 def alias():
-    """ Custom commands and the functions they map too """
-    #  This is a required portion of the commands to actually
-    #  load all of the variations and new methods. Your commands
-    #  can leverage the kwargs object. This will include wether
-    #  not your user is banned or an admin.
+    ''' Custom commands and the functions they map too '''
     alias = dict(
             mypoints=my_points,
             scoreboard=all_points,
@@ -15,52 +11,38 @@ def alias():
 
 
 def my_points(**kwargs: dict) -> str:
-    """ Get user FakeInternetPoints count for user """
+    ''' Get user FakeInternetPoints count for user '''
     import pickle
     user = kwargs['message'].user
-    try:
-        _scorefile = open('data/score', 'rb')
-        _scoredict = pickle.load(_scorefile)
-        _scorefile.close()
-    except Exception:
-        _scoredict = {}
-    if user in _scoredict:
-        msg = '<@{0}> has {1} points'.format(user, _scoredict[user])
+    score = kwargs.get('score')
+    if user in score:
+        msg = '<@{0}> has {1} points'.format(user, score[user])
     else:
         msg = "<@{0}> doesn't have points".format(user)
     return msg
 
 
 def all_points(**kwargs: dict) -> str:
-    """ Get a sorted list of fakeinternetpoints for team; requires admin """
-    import pickle
+    ''' Get a sorted list of fakeinternetpoints for team; requires admin '''
+    score = kwargs.get('score')
     msg = ''
     if not kwargs['message'].admin:
         return msg
-    try:
-        _scorefile = open('data/score', 'rb')
-        _scoredict = pickle.load(_scorefile)
-        _scorefile.close()
-    except Exception:
-        return msg
-    if _scoredict == {}:
+    if scored == {}:
         msg = 'No one on your team has gained or lost any fake internet points'
-    sorted_score = sorted(_scoredict, key=_scoredict.__getitem__)
+    sorted_score = sorted(score, key=score.__getitem__)
     sorted_score.reverse()
     for user in sorted_score:
-        msg += "<@{0}> has a score of {1}\n".format(user, _scoredict[user])
+        msg += "<@{0}> has a score of {1}\n".format(user, scored[user])
     return msg
 
 
 def reset_points(**kwargs: dict) -> str:
-    """ Resets scoreboard for entire team; requires admin """
-    import pickle
+    ''' Resets scoreboard for entire team; requires admin '''
+    score = kwargs.get('score')
     msg = ''
     if not kwargs['message'].admin:
         return msg
-    _mydict = {}
-    _scorefile = open('data/score', 'wb')
-    pickle.dump(_mydict, _scorefile)
-    _scorefile.close()
+    score = {}
     msg = "All scores have been reset"
     return msg
