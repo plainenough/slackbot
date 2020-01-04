3c53e642246c4936a3db0fbe755c6579507ba5ee
Derrick Walton <derrick@tosstalk.com>
2020-01-03 21:05:35 -0700

    fix bug where no checking banned list

--- commands/ban.py
+++ commands/ban.py
@@ -12,7 +12,7 @@ def ban_user(**kwargs: dict) -> str:
     """ban_user will ban a user from using commands"""
     user = kwargs.get('user')
     message = kwargs.get('message')
-    banned = kwargs.get('banned')
+    banned = message._kwargs.get('banned')
     if user == 'none':
         return ''
     if message.admin:
@@ -27,7 +27,7 @@ def ban_user(**kwargs: dict) -> str:
 def unban_all(**kwargs: dict) -> str:
     """unban_all will unban all users"""
     message = kwargs.get('message')
-    banned = kwargs.get('banned')
+    banned = message._kwargs.get('banned')
     if message.admin:
         banned.clear()
         msg = 'Cleared the ban list'
@@ -41,7 +41,7 @@ def unban_user(**kwargs: dict) -> str:
     """unban_user will lift an existing user ban"""
     user = kwargs.get('user')
     message = kwargs.get('message')
-    banned = kwargs.get('banned')
+    banned = message._kwargs.get('banned')
     if message.admin:
         banned.pop(user, None)
         msg = 'User <@{0}> is unbanned.\n'.format(user)
