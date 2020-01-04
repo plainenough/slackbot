bd676eec388022bd810b800d4a87445a09ee07d3
Derrick Walton <derrick@tosstalk.com>
2020-01-03 21:04:41 -0700

    clean up useless comments from help

--- commands/help.py
+++ commands/help.py
@@ -3,10 +3,6 @@
 
 def alias():
     """ Custom commands and the functions they map too """
-    #  This is a required portion of the commands to actually
-    #  load all of the variations and new methods. Your commands
-    #  can leverage the kwargs object. This will include wether
-    #  not your user is banned or an admin.
     alias = dict(help=generate_help)
     return alias
 
