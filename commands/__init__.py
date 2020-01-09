f454ebd31bd8584e6f24e0b7fa334c1ac273821f
Derrick Walton <derrick@tosstalk.com>
2020-01-08 21:32:23 -0700

    add fix for __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -7,28 +7,33 @@ def discover_commands(logging):
         of the predefined command aliases for each command. This
         will be templated and should be easy to understand.
     """
-    import importlib
     file_list = filter_list(logging)
     commands = {}
     logging.debug("Checking files in file list for py extention.")
     for item in file_list:
-        _aliases = {}
         logging.debug("Checking file: {0}".format(item))
         if item.split('.')[1] == 'py':
             if __name__ == '__main__':
                 _item = '{0}'.format(item.split('.')[0])
             else:
                 _item = 'commands.{0}'.format(item.split('.')[0])
-            _newcommand = importlib.import_module(_item)
-            try:
-                _aliases = _newcommand.alias()
-            except Exception as e:
-                logging.info("{0} did not have an alias function.".format(
-                    _item))
-                logging.debug(e)
-            logging.debug(_aliases)
-            for alias, function in _aliases.items():
-                commands[alias] = function
+            check_alias(logging, _item, commands)
+    return commands
+
+
+def check_alias(logging, _item, commands):
+    import importlib
+    _aliases = {}
+    _newcommand = importlib.import_module(_item)
+    try:
+        _aliases = _newcommand.alias()
+    except Exception as e:
+        logging.info("{0} did not have an alias function.".format(
+            _item))
+        logging.debug(e)
+    logging.debug(_aliases)
+    for alias, function in _aliases.items():
+        commands[alias] = function
     return commands
 
 
