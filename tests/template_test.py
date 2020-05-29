ca60ff2b51b58c41bea44b339ed0144d3e1ce8ee
Derrick Walton <derrick.e.walton@gmail.com>
2020-05-29 09:56:07 -0600

    add docstrings for template_test.py

--- tests/template_test.py
+++ tests/template_test.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python3
+"""Example template test for new commands."""
+
+
 import pytest
 
 
 class Message(object):
-    """ A test class """
+    """Generate simple message object."""
 
     def __init__(self):
+        """Initialize simple message object."""
         self.user = "bob"
 
 
 def test_alias():
+    """Test functionality of alias command."""
     from commands import template
     commands = template.alias()
     assert 'mycommand' in commands
 
 
 def test_templateuserreturn():
+    """Test template user return functionality."""
     from commands import template
     message = Message()
     kwargs = dict(message=message)
