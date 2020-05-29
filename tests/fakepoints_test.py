c3c3773b288f7e169f2d85e5074ca2b99f635ac5
Derrick Walton <derrick.e.walton@gmail.com>
2020-05-29 09:25:19 -0600

    add docstrings for fakepoints_test.py

--- tests/fakepoints_test.py
+++ tests/fakepoints_test.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
+"""Test FakeInternetPoints functionality."""
+
+
 import pytest
 
 
 class Message(object):
-    ''' Test class for command test '''
+    """Define simple message object for testing."""
 
     def __init__(self):
+        """Initialize simple message object."""
         self._text = '<@FRED> ++ for good work homie'
         self.channel = '#general'
         self.user = 'bob'
@@ -16,6 +19,7 @@ class Message(object):
 
 @pytest.fixture
 def fixture_fip():
+    """Fixture for adding fake points."""
     import fake_points
     message = Message()
     #  Writing an empty score file
@@ -27,6 +31,7 @@ def fixture_fip():
 
 @pytest.fixture
 def fixture_fip_self():
+    """Fixture for adding points to self."""
     import fake_points
     message = Message()
     #  Writing an empty score file
@@ -40,6 +45,7 @@ def fixture_fip_self():
 
 @pytest.fixture
 def fixture_fip_negative():
+    """Fixture for removing fake points."""
     import fake_points
     message = Message()
     #  Writing an empty score file
@@ -52,6 +58,7 @@ def fixture_fip_negative():
 
 @pytest.fixture
 def fixture_fip_gtv():
+    """Fixture for adding/removing too many fake points."""
     import fake_points
     message = Message()
     #  Writing an empty score file
@@ -65,16 +72,19 @@ def fixture_fip_gtv():
 
 
 def test_awarder(fixture_fip):
+    """Test target user."""
     fip = fixture_fip
     assert fip.awarder == 'bob'
 
 
 def test_change(fixture_fip):
+    """Test point increment."""
     fip = fixture_fip
     assert fip.change == 1
 
 
 def test_message(fixture_fip):
+    """Test message for correct format."""
     fip = fixture_fip
     _msg1 = "<@FRED> has changed by 1 "
     _msg2 = ", now they have 1 in total.\n"
@@ -83,17 +93,20 @@ def test_message(fixture_fip):
 
 
 def test_awarder_self(fixture_fip_self):
+    """Test functionality about adding points to ones self."""
     fip = fixture_fip_self
     msg = "<@bob> You are not allowed to assign yourself points."
     assert fip.msg == msg
 
 
 def test_change_gtv(fixture_fip_gtv):
+    """Test point change limitations."""
     pfip, nfip = fixture_fip_gtv
     assert pfip.change == 5
     assert nfip.change == -5
 
 
 def test_change_negative(fixture_fip_negative):
+    """Test functionality for negative additions."""
     fip = fixture_fip_negative
     assert fip.change == -1
