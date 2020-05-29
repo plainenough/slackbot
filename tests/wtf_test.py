6c6e5cf4d343507dac532df12a5c33629dc7160f
Derrick Walton <derrick.e.walton@gmail.com>
2020-05-29 09:56:25 -0600

    add docstrings for wtf_test.py

--- tests/wtf_test.py
+++ tests/wtf_test.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
+"""Test suite for wtf.py."""
+
+
 import pytest
 
 
 class Message(object):
-    ''' Test class for command test '''
+    """Create simple message object for tests."""
 
     def __init__(self):
+        """Initialize simple object for tests."""
         self._text = 'test because this string is too long to lookup'
         self.channel = '#general'
         self.user = 'bob'
@@ -13,6 +16,7 @@ class Message(object):
 
 @pytest.fixture
 def fixture_wtf():
+    """Fixture for wtf tests."""
     from commands import wtf
     message = Message()
     kwargs = dict(user=message.user,
@@ -23,6 +27,7 @@ def fixture_wtf():
 
 
 def test_too_long(fixture_wtf):
+    """Test for long strings."""
     message, text = fixture_wtf
     msg = 'Search term "{0}" is too long. '
     msg += "keep it under 30."
